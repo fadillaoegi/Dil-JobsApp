dab47a2c1a5c1b6c3e84af808e727dd1593b867a
fadillaoegi <fadilfdl2710@gmail.com>
2023-08-29 01:47:40 +0700

    refactoring

---
+++ lib/apis/api_config.dart
@@ -0,0 +1,8 @@
+class ApiConfigDilJob {
+  // Main Api
+  static String mainApi = "https://future-jobs-api.vercel.app";
+
+  static String apiAuthRegister = "$mainApi/register";
+  static String apiAuthLogin = "$mainApi/login";
+  static String apiGetJob = "$mainApi/jobs";
+}
